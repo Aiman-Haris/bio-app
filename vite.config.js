f2f522b6724e65ec88bfb0133e70ed03a84be7db
Aiman-Haris <muhdaimanharis@gmail.com>
2026-01-10 21:54:34 +0800

    v

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bio-app/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react()
